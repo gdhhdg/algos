b9271cda0d138cfcf8eed095da114831cca1b3d3
Gunner <gdhatmaker@gmail.com>
2019-05-19 08:19:50 +0700

    Update bubble_sort.js
    Added largest to smallest. Comments.

--- bubble_sort.js
+++ bubble_sort.js
@@ -4,7 +4,7 @@
 let array1 = [4,11,2,47,7,34,65,69,9,37];
 let array2 = [74,8,15,25,26,40,25,34,42,71,1,18];
 let array3 = [1,2,12,62,75,77,6,13,72,77,2,62,69,70];
-
+//smallest to largest
 bubble_sort = (array) => {
     for (let x = array.length; x >=0; x++){
         let i = 0;
@@ -23,7 +23,27 @@ bubble_sort = (array) => {
         };
     }
     console.log(array);
-    return array;
 };
 
-bubble_sort(array3);
+//largest to smallest
+bubble_sort_reverse = (array) => {
+    for (let x = array.length; x >=0; x++){
+        let i = 0;
+        while (i <= array.length){
+            if (array[i] < array[i+1]){
+                let bigger_index = i;
+                let pulled = array[i];
+                array.splice(i,1);
+                array.splice(bigger_index + 1 , 0, pulled);
+                console.log(array);
+                i++;
+            }
+            else {
+                i++;
+            }
+        };
+    }
+    console.log(array);
+};
+
+bubble_sort_reverse(array3);
